0b9bab96d626b5f173e75637ff3cf7af2dff9151
Manjilan <nakarmimanjila@yahoo.com>
2019-01-25 21:46:37 -0800

    created Starter component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Starter Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
