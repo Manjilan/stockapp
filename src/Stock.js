80cf4a0c59c3d0a76541663922eed1ebf3529280
Manjilan <nakarmimanjila@yahoo.com>
2019-01-28 22:41:34 -0800

    stock component

--- src/Stock.js
+++ src/Stock.js
@@ -4,7 +4,12 @@ import './StockList.css';
 class Stock extends Component {
   render() {
     return (
-      <div>
+      <div className="element">
+        <img src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80"/>
+        <div>
+          <p>Test</p>
+          <p>NSDAQ</p>
+        </div>
       </div>
     );
   }
--- src/StockList.js
+++ src/StockList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Stock from "./Stock";
 import './StockList.css';
 
 class StockList extends Component {
   render() {
     return (
       <div>
+        <Stock />
       </div>
     );
   }
